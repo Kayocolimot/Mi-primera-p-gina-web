0ef18c5a3ea4cb299e441381d32ec082a990cc2e
Kayocolimot <kayocolimot@gmail.com>
2025-09-12 20:37:31 -0600

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Visita https://openweathermap.org/api
 API_KEY = "c4ccb2d40ebe51a4316672a63b9a3553"
 
-@app.route('/weather')
+@app.route('/weather', methods=['GET'])
 def get_weather():
     city = request.args.get('city')
     if not city:
